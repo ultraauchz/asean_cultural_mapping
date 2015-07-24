1bd1eb30d0c0a4737a221db21dc2bce207bde212
unknown <Teerawat@.(none)>
2015-07-24 15:51:42 +0700

    update

--- application/views/layouts/admin/layout.php
+++ application/views/layouts/admin/layout.php
@@ -1,65 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <base href="<?php echo base_url()?>" ></base>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo $template["title"]?></title>
-
-    <!-- Bootstrap -->
-    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css" >
-	<link type="text/css" rel="stylesheet" href="js/fancybox/jquery.fancybox.css" media="screen" />
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><?php include_once '_meta.php'; ?></head>
+<body class="skin-blue">
+    <div class="wrapper">
+      
+      <header class="main-header">
+      	<?php include_once '_header.php';?>
+      </header>
     
-    <!-- jquery_validate -->
-    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
-     
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/function.js"></script>
-
-	<script type="text/javascript" src="js/fancybox/jquery.fancybox.pack.js" ></script>
-	<script type="text/javascript">
-		$('.iframe-btn').fancybox({	
-			'width'		: 800,
-			'height'		: 600,
-			'type'			: 'iframe',
-	        'autoSize'  	: false
-	    });
-	</script>
-	
-	<?php echo $template['metadata']; ?>
-	
-	<style type="text/css">
-		ul.pagination li span.current {
-			z-index: 2;
-			color: #fff;
-			cursor: default;
-			background-color: #337ab7;
-			border-color: #337ab7;
-		}
-	</style>
-
-</head>
-
-<body>
-
-    <?php echo modules::run("admin/inc_menu");?>
-
-    <div class="container" >
-        <?php echo $template["body"]?>  
-    </div>
+      <aside class="main-sidebar">
+      	<?php include_once '_menu.php';?>
+      </aside>
+    
+      <!-- Content Wrapper. Contains page content -->
+      <div class="content-wrapper">
+      	<?php echo $template['body']; ?>
+      </div><!--end Content Wrapper-->
+    
+    	<footer class="main-footer">
+        <?php include_once '_footer.php'; ?>
+        </footer>
+    
+    </div><!--end wrapper;-->
+                                  
 </body>
-
 </html>
