037b604bae7cae5e872dd2147ff784347b8062e5
unknown <Teerawat@.(none)>
2015-08-17 11:29:50 +0700

    a

--- application/modules/admin/views/heritages/index.php
+++ application/modules/admin/views/heritages/index.php
@@ -3,37 +3,53 @@
 <div class="row">
     <div class="col-xs-12">
 		<div class="box">
-			<table class="table table-bordered table-hover table-responsive table-striped" >
+			<form method="get" enctype="multipart/form-data">
+			<div class="box-header">
+			  <h3 class="box-title">Search</h3>			  
+			</div><!-- /.box-header -->
+			<div style="float:left;width:100%;">
+			  <div class="col-xs-3">
+			  	<label for="search">Title</label> 
+			  	<input type="text" name="search" class="form-control" placeholder="Enter Heritage Title" value="<?=@$_GET['search'];?>">
+			  </div>
+			  <div class="col-xs-3">
+			  	<label for="coutry_id">Country</label> 
+			  	<?php echo form_dropdown("country_id",get_option("id","country_name","acm_country"," ORDER BY country_name ASC"),@$_GET["country_id"],"class=\"form-control\" style=\"display:inline;\" ","-- Select Country --","")?>
+			  </div>
+			  <div class="col-xs-3">
+			  	<br>
+			  	<input type="submit" name="b" class="btn btn-primary" value="Search">
+			  </div>
+			</div>
+			</form>
+			
+			<div class="box-body">
+			<?php echo $variable->pagination()?>
+			<table id="example1" class="table table-bordered table-striped table-hover table_data">
 				<thead>
 					<tr>
-						<th style="width: 80px;" >SHOW NO</th>
-						<th style="width: 80px;" >STATUS</th>
+						<th style="width:70px;">NO</th>
 						<th>TITLE</th>
+						<th>COUNTRY</th>
 						<th style="width: 160px;" >CREATED/UPDATED DATE</th>
-						<th style="width: 180px;" >
-							<a href="admin/<?php echo $modules_name;?>/form" class="btn btn-info"><li class="fa fa-plus"></li> Create new</a>
-						</th>
+						<th class="th_manage">Manage</th>						
 					</tr>
 				</thead>
 				
 				<tbody>
-					<?php foreach ($variable as $key => $value):?>
+					<?php 
+						foreach ($variable as $key => $value):
+							$no++;
+					?>
 					<tr>
+						<td><?php echo $no;?></td>						
 						<td>
-							<input type="text" class="form-control" name="orders[]" value="<?php echo $value->orders?>" style="text-align: center; width: 45px;" />
-							<input type="hidden" name="id[]" value="<?php echo $value->id?>" />
+							<?php echo $value->title?>							
 						</td>
 						<td>
-							<button type="button" id="<?php echo $value->id?>" class="btn <?php echo ($value->status==1) ? "btn-primary" : "btn-danger" ?>" data-loading-text="บันทึก..." value="<?php echo ($value->status==1) ? 1 : 0 ?>"  >
-								<?php echo ($value->status==1) ? "On" : "Off" ?>
-							</button>
+							<?php echo $value->country->country_name;?>
 						</td>
-						<td>
-							<strong><?php echo $value->title?></strong>
-							<hr />
-							<img src="<?php echo $value->image_path?>" class="img-responsive" style="max-height: 150px;" />
-						</td>
-						<td><small><?php echo mysql_to_th($value->created,"S",TRUE)."<br />".mysql_to_th($value->updated,"S",TRUE)?></small></td>
+						<td><small><?php echo $value->created."<br />".$value->updated;?></small></td>
 						<td>
 							<a href="admin/heritages/form/<?php echo $value->id?>" class="btn btn-primary" ><span class="glyphicon glyphicon-wrench" ></span> Edit</a>
 							<a href="admin/heritages/delete/<?php echo $value->id?>" class="btn btn-danger" onclick="return confirm('ต้องการลบ <?php echo $value->title?> หรือไม่')" ><span class="glyphicon glyphicon-trash" ></span> Delete</a>
@@ -41,48 +57,22 @@
 					</tr>
 					<?php endforeach?>
 				</tbody>
+				<tfoot>
+					<tr>
+						<th>NO</th>
+						<th>TITLE</th>
+						<th>COUNTRY</th>
+						<th style="width: 160px;" >CREATED/UPDATED DATE</th>
+						<th class="th_manage">Manage</th>						
+					</tr>
+				</tfoot>
 			</table>
-			<!-- <div style="text-align:right;">
+			<div style="text-align:right;">
 			  	<a href="admin/<?php echo $modules_name;?>/form" class="btn btn-info"><li class="fa fa-plus"></li> Create new</a>
-			  </div> -->
+			</div>
 			<?php echo $variable->pagination()?>
+			</div>
 		</div>
 	</div>
 </div>	
 </section>
-
-
-
-<script type="text/javascript">
-	$(document).ready(function(){
-		
-		$('button[data-loading-text]').click(function () {
-		    var btn = $(this);
-		    if(btn.val()==1) {
-				btn.val(0);
-		    	btn.removeClass("btn-primary");
-		    	btn.addClass("btn-danger");
-		    	var status = 0;
-		    	var textstatus = "Off";
-		    } else {
-				btn.val(1);
-		    	btn.removeClass("btn-danger");
-		    	btn.addClass("btn-primary");
-		    	var status = 1;
-		    	var textstatus = "On";
-		    }
-		    btn.button('loading');
-		    
-		    setTimeout(function(){
-				btn.button('reset');
-				btn.html(textstatus);
-		    },1000);
-		    
-		    var id = btn.attr("id");
-		    $.post("admin/approve/heritage/"+id,{status:status});
-		    return false;
-		    
-		});
-		
-	});
-</script>
